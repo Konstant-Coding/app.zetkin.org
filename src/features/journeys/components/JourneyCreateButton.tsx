ca57be718eddecec27325fdf513ca7cc36ec2a7c
Konstant-Coding <konstantin.papirnik@3-s.at>
2026-03-28 11:50:24 +0100

    Add CreateActionButton component for event management actions

---
+++ src/features/journeys/components/JourneyCreateButton.tsx
@@ -0,0 +1,191 @@
+import { useRouter } from 'next/router';
+import { Box, Button } from '@mui/material';
+import {
+  ArrowForward,
+  CancelOutlined,
+  ContentCopy,
+  Delete,
+  RestoreOutlined,
+} from '@mui/icons-material';
+import React, { useContext, useState } from 'react';
+import dayjs from 'dayjs';
+
+import messageIds from '../l10n/messageIds';
+import useDuplicateEvent from '../hooks/useDuplicateEvent';
+import useEventMutations from '../hooks/useEventMutations';
+import { useMessages } from 'core/i18n';
+import { ZetkinEvent } from 'utils/types/zetkin';
+import { ZUIConfirmDialogContext } from 'zui/ZUIConfirmDialogProvider';
+import ZUISnackbarContext from 'zui/ZUISnackbarContext';
+import ZUIDatePicker from 'zui/ZUIDatePicker';
+import ZUIEllipsisMenu from 'zui/ZUIEllipsisMenu';
+import ChangeCampaignDialog from '../../campaigns/components/ChangeCampaignDialog';
+
+interface CreateActionButtonProps {
+  event: ZetkinEvent;
+}
+
+// TODO: Add mui box and inside add mui buton, code taken from EventLayout
+const CreateActionButton: React.FunctionComponent<CreateActionButtonProps> = ({
+  event,
+}) => {
+  const messages = useMessages(messageIds);
+  const orgId = event.organization.id;
+  const { showConfirmDialog } = useContext(ZUIConfirmDialogContext);
+  const { cancelEvent, updateEvent, deleteEvent, restoreEvent, setPublished } =
+    useEventMutations(orgId, event.id);
+  const { showSnackbar } = useContext(ZUISnackbarContext);
+  const router = useRouter();
+  const duplicateEvent = useDuplicateEvent(orgId, event.id);
+  const [isMoveDialogOpen, setIsMoveDialogOpen] = useState(false);
+
+  const published =
+    !!event.published && new Date(event.published) <= new Date();
+  const handlePublish = () => {
+    setPublished(new Date().toISOString());
+  };
+
+  const handleUnpublish = () => {
+    setPublished(null);
+  };
+  const handleChangeDate = (date: string | null) => {
+    const newDateIsDifferent =
+      date && date != dayjs(event.published).format('YYYY-MM-DD');
+
+    if (newDateIsDifferent) {
+      setPublished(date);
+    }
+
+    if (!date) {
+      setPublished(null);
+    }
+  };
+
+  const handleDelete = () => {
+    deleteEvent();
+    router.push(`/organize/${orgId}/projects/${event.campaign?.id || ''} `);
+  };
+
+  const handleDuplicate = () => {
+    duplicateEvent();
+  };
+
+  const handleCancel = () => {
+    if (event.cancelled) {
+      restoreEvent();
+    } else {
+      cancelEvent();
+    }
+  };
+
+  const handleMove = () => {
+    setIsMoveDialogOpen(true);
+  };
+
+  const handleOnCampaignSelected = async (campaignId: number) => {
+    const updatedEvent = await updateEvent({ campaign_id: campaignId });
+    await router.push(
+      `/organize/${orgId}/projects/${campaignId}/events/${event.id}`
+    );
+    showSnackbar(
+      'success',
+      messages.eventChangeCampaignDialog.success({
+        campaignTitle: updatedEvent.campaign?.title || '',
+        eventTitle:
+          updatedEvent.title ||
+          updatedEvent.activity?.title ||
+          messages.common.noTitle(),
+      })
+    );
+  };
+
+  return (
+    <Box alignItems="flex-end" display="flex" flexDirection="column" gap={1}>
+      <Box>
+        {published ? (
+          <Button onClick={handleUnpublish} variant="outlined">
+            {messages.create.unpublish()}
+          </Button>
+        ) : (
+          <Button onClick={handlePublish} variant="contained">
+            {messages.eventActionButtons.publish()}
+          </Button>
+        )}
+        <ZUIEllipsisMenu
+          items={[
+            {
+              label: event.cancelled
+                ? messages.eventActionButtons.restore()
+                : messages.eventActionButtons.cancel(),
+              onSelect: () => {
+                showConfirmDialog({
+                  onSubmit: handleCancel,
+                  title: event.cancelled
+                    ? messages.eventActionButtons.restore()
+                    : messages.eventActionButtons.cancel(),
+                  warningText: event.cancelled
+                    ? messages.eventActionButtons.warningRestore({
+                        eventTitle:
+                          event.title ||
+                          event.activity?.title ||
+                          messages.common.noTitle(),
+                      })
+                    : messages.eventActionButtons.warningCancel({
+                        eventTitle:
+                          event.title ||
+                          event.activity?.title ||
+                          messages.common.noTitle(),
+                      }),
+                });
+              },
+
+              startIcon: event.cancelled ? (
+                <RestoreOutlined />
+              ) : (
+                <CancelOutlined />
+              ),
+            },
+            {
+              label: <>{messages.eventActionButtons.duplicate()}</>,
+              onSelect: handleDuplicate,
+              startIcon: <ContentCopy />,
+            },
+            {
+              label: <>{messages.eventActionButtons.move()}</>,
+              onSelect: handleMove,
+              startIcon: <ArrowForward />,
+            },
+            {
+              label: <>{messages.eventActionButtons.delete()}</>,
+              onSelect: () => {
+                showConfirmDialog({
+                  onSubmit: handleDelete,
+                  title: messages.eventActionButtons.delete(),
+                  warningText: messages.eventActionButtons.warning({
+                    eventTitle:
+                      event.title ||
+                      event.activity?.title ||
+                      messages.common.noTitle(),
+                  }),
+                });
+              },
+              startIcon: <Delete />,
+            },
+          ]}
+        />
+      </Box>
+      <Box>
+        <ZUIDatePicker date={event.published} onChange={handleChangeDate} />
+      </Box>
+      <ChangeCampaignDialog
+        errorMessage={messages.eventChangeCampaignDialog.error()}
+        onCampaignSelected={handleOnCampaignSelected}
+        onClose={() => setIsMoveDialogOpen(false)}
+        open={isMoveDialogOpen}
+        title={messages.eventChangeCampaignDialog.title()}
+      />
+    </Box>
+  );
+};
+
+export default CreateActionButton;
